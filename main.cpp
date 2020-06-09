08af0284eda74f8a7efebf0efdc623145eb938be
Thu Nguyen <tqn14@tamu.edu>
2020-06-08 18:50:42 -0700

    finished

--- main.cpp
+++ main.cpp
@@ -12,19 +12,79 @@
 using namespace std; 
 
 void display(vector<DLList<Record>> &v) {
-    for (int i=0; i<v.size(); ++i) {
-        cout<<"List: "<<i<<"\n"<<v[i];
-        cout<<endl<<endl;
+    for (auto a : v) { 
+        cout << a; 
+        cout << endl; 
     }
 }
 
 void insert(vector<DLList<Record>> &v, vector<Record> &book) {
     for (int i = 0; i <book.size(); i++) {
-        int num = book[i].getTitle().at(0) - 65; 
+        int num = toupper(book[i].getTitle().at(0)) - 65; 
         v[num].insert_last(book[i]); 
+    } 
+}
+void search(vector<DLList<Record>> &v) {
+    vector<Record> title; 
+    string ttl; 
+    cout << "Enter the name to search: "; 
+    getline(cin, ttl); 
+    ttl[0] = (toupper(ttl[0]));
+    int num = ttl[0] - 65; 
+    DLListNode<Record>* tmp = v[num].first_node();
+    for_each(ttl.begin(), ttl.end(), [](char &ttl){
+	        ttl = ::tolower(ttl);
+        });
+    while(tmp!=v[num].after_last_node()) {
+        string obj = tmp->getObj().getTitle();  
+        for_each(obj.begin(), obj.end(), [](char &obj){
+	        obj = ::tolower(obj);
+        });
+        if (obj.compare(ttl) == 1)  {
+            title.push_back(tmp->getObj());
+        }
+        tmp = tmp->getNext();
+    }
+    if (title.size() == 0) {
+        cout << "There is no record of this title. Please add this title to the database." << endl; 
+        string author, isbn, year, edt; 
+        cout << "Please enter the author of the book: "; 
+        getline(cin, author); 
+        cout << "Please enter its isbn : "; 
+        getline(cin, isbn); 
+        cout << "Please enter publication year: "; 
+        getline(cin, year); 
+        cout << "Please enter edition number: "; 
+        getline(cin, edt); 
+        
+        Record temp_rc(ttl, author, isbn, year, edt);
+        title.push_back(temp_rc); 
+        insert(v, title); 
+        cout << "\nThe database is now updated\n"<< endl; 
+        display(v); 
+        search(v);
+        
+    } else if (title.size() == 1) {
+        cout << "Here it is: " << endl << endl; 
+        cout << title[0]; 
+    } else {
+        cout << "There are multiple records of the same name" << endl << endl; 
+        for (auto a : title) {
+            cout << a; 
+            cout << endl; 
+        }
+        char edt; 
+        cout << "\nPlease enter edition number (1 or 2, etc.): "; 
+        cin >> edt; 
+        for (auto a : title) {
+            if (a.getEdtNum().at(0) == edt) {
+                cout << endl;
+                cout << a; 
+                cout << endl; 
+            }
+        }
     }
 }
-
 
 int main() {
     vector<Record> rc; 
@@ -49,8 +109,10 @@ int main() {
     // for (int i = 0; i < rc.size(); i++) {
     //     os << rc[i] << endl; 
     // }
-    cout << "AFTER INSERT *******" << endl; 
-    cout << br[7]; 
+    // cout << "AFTER INSERT *******" << endl; 
+    //cout << br[7]; 
     
+    search(br); 
+    return 0;
  
 }
